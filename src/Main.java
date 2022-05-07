34730e87c9615b60f10745d27279c245cd789cff
HiroshiHara <mrbob403@gmail.com>
2022-05-07 14:41:01 +0900

    不要なimport文を削除

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionAdapter;
-import java.awt.event.MouseMotionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-import java.awt.event.WindowListener;
 
 import javax.swing.Box;
 import javax.swing.BoxLayout;
